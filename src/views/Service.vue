6fd1ef9ada7d2db605555565d996c8ad1805e575
Heraldo Yusron <heraldoyusrontris@gmail.com>
2019-06-14 18:20:13 +0700

    service router page

---
+++ src/views/Service.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>This is an service page</h1>
+    <p>by Heraldo Yusron</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'service'
+}
+</script>
