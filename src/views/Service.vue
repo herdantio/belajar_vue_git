eb86da159aea49ae10c66c0ba3767804c573f66a
herdantio <herdantio.sindhunata@binus.ac.id.com>
2019-06-14 18:13:38 +0700

    ADD service page

---
+++ src/views/Service.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Service Page</h1>
+        <p>This is a service page</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Service"
+    }
+</script>
+
+<style scoped>
+
+</style>
